defffb06374ebe31f2cb153ef1680ce533463d02
elsayednofal <elsayed_nofal@ymail.com>
2017-02-13 15:19:37 +0200

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['namespace'=>'SayedNofal\ImageManager\Http\Controllers'],function(){
+Route::group(['namespace'=>'Elsayednofal\Imagemanager\Http\Controllers'],function(){
     
     Route::post('image-manager/upload','ImageManagerController@uplooadImage');
     Route::get('image-manager/get-images/{keyword?}','ImageManagerController@getImages');
