b7250f96e98e8d3896e894c12222509413be4121
elsayednofal <elsayed_nofal@ymail.com>
2017-02-13 15:20:24 +0200

    Update ImageManagerController.php

--- src/Http/Controllers/ImageManagerController.php
+++ src/Http/Controllers/ImageManagerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SayedNofal\ImageManager\Http\Controllers;
+namespace Elsayednofal\Imagemanager\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
