69434a4b84183541ca1e44a1abf8362e9b9aa1ba
Elsayed Nofal <elsayed_nofal@ymail.com>
2018-05-20 14:10:25 +0200

    Update selector.blade.php

--- src/resources/views/selector.blade.php
+++ src/resources/views/selector.blade.php
@@ -1,6 +1,6 @@
 <!-- Button trigger modal -->
 <?php
-        $file_system=new Elsayednofal\Imagemanager\Http\Controllers\FileSystem();
+        $file_system=new \Elsayednofal\Imagemanager\Http\Controllers\FileSystem();
         $file_system=$file_system->init();
 ?>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
