c4e1ad2ee1f46c5b13d4b348be6ffa0a25bd7b9c
Elsayed Nofal <elsayed_nofal@ymail.com>
2019-09-02 13:00:31 +0200

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,6 +5,6 @@ Route::group(['namespace'=>'Elsayednofal\Imagemanager\Http\Controllers'],functio
     Route::any('image-manager/upload','ImageManagerController@uplooadImage');
     Route::any('image-manager/done','ImageManagerController@doneImage');
     Route::get('image-manager/get-images/{keyword?}','ImageManagerController@getImages');
-    Route::post('image-manager/crop','ImageManagerController@cropImage');
+//     Route::post('image-manager/crop','ImageManagerController@cropImage');
 });
 
